ec8418087e5aa44d4373a12d9178b20d9f86be4d
michaleb <michale.bailey@gmail.com>
2020-12-09 17:36:55 -0500

    Completed time format function

--- src/format.cpp
+++ src/format.cpp
@@ -7,5 +7,20 @@ using std::string;
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+
+string Format::ElapsedTime(long seconds) { 
+    
+    string H, M, S, h, m, s;
+
+    h = std::to_string(seconds/3600);
+    m = std::to_string((seconds%3600)/60);
+    s = std::to_string(seconds%60);
+
+    //Prepend additional '0' to always return the HH:MM:SS time format
+    //For uptime >= 100 hours formatting adjusts to HHH, HHHH, etc..
+    h.length()<2 ? (H = string(2 - h.length(), '0') + h) : H = h;
+    M = string(2 - m.length(), '0') + m;
+    S = string(2 - s.length(), '0') + s;
+
+    return (H+":"+M+":"+S);
+}
